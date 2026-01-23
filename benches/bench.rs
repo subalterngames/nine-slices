6b00c96f1cbb8baf9bde00227b630a7f1bad259a
Esther Alter <subalterngames@gmail.com>
2026-01-23 15:57:40 -0500

    ok it's a small improvement

--- benches/bench.rs
+++ benches/bench.rs
@@ -37,6 +37,19 @@ fn criterion_benchmark(c: &mut Criterion) {
     });
 
     let mut sprite = NineSlicedSprite::from_png(
+        Cursor::new(include_bytes!("../test_files/src/solid.png")),
+        slices,
+        BorderScaling::Stretch,
+    )
+    .unwrap();
+    sprite.set_resize_algorithm(ResizeAlg::Nearest);
+    c.bench_function("stretched borders filled", |b| {
+        b.iter(|| {
+            let _ = sprite.resize(1024, 768).unwrap();
+        })
+    });
+
+    let mut sprite = NineSlicedSprite::from_png(
         Cursor::new(include_bytes!("../test_files/src/repeat.png")),
         slices,
         BorderScaling::Repeat,
